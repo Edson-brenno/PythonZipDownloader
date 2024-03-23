43837f4a41c80698551c2c9e340c9f5805fba3f9
brenno <brennorebeiro@gmail.com>
2024-03-23 20:09:09 -0300

    Add validation lin

--- zip_download.py
+++ zip_download.py
@@ -1,4 +1,5 @@
 import io
+import pyping2
 from urllib import request
 
 
@@ -7,21 +8,28 @@ class ZipDownload:
         self.__link = None
         self.__filename = None
 
+    def __is_link_valid(self, link) -> bool:
+        total_response_ping = pyping2.ping(link)
+        if total_response_ping[1] == 200:
+            return True
+        else:
+            return False
+
     def perguntar_link(self) -> None:
         while True:
             try:
                 link_inputed = str(input("Digite o link: "))
+                # check if the link starts with http:// or https://
                 if link_inputed.startswith("http://") or link_inputed.startswith("https://"):
-                    self.__link = link_inputed
-                    break
+                    # Check if the link is valid
+                    if self.__is_link_valid(link_inputed):
+                        self.__link = link_inputed
+                        break
+                    else:
+                        # raise error
+                        raise Exception("Link inválido")
                 else:
+                    # raise error
                     raise Exception("Error link invalido! O link deve comecar com http:// or https://")
             except Exception as e:
                 print(e)
-
-    def validar_link(self):
-
-        if self.__link.startswith("http://") or self.__link.startswith("https://"):
-            pass
-        else:
-            pass
