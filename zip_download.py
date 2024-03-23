3c96d0b946ecc9dd5d3535b1506b4231ca6baee5
brenno <brennorebeiro@gmail.com>
2024-03-23 20:42:51 -0300

    Add validation

--- zip_download.py
+++ zip_download.py
@@ -7,6 +7,7 @@ class ZipDownload:
     def __init__(self):
         self.__link = None
         self.__filename = None
+        self.__output_file = None
 
     def __is_link_valid(self, link) -> bool:
         total_response_ping = pyping2.ping(link)
@@ -19,11 +20,20 @@ class ZipDownload:
         while True:
             try:
                 link_inputed = str(input("Digite o link: "))
-                # check if the link starts with http:// or https://
-                if (link_inputed.startswith("http://") and link_inputed.endswith(".zip") or
-                        link_inputed.startswith("https://") and link_inputed.endswith(".zip")):
+                if ((link_inputed.startswith("http://") and link_inputed.endswith(".zip")) or
+                        (link_inputed.startswith("https://") and link_inputed.endswith(".zip")) or
+                        (link_inputed.startswith("http://") and link_inputed.endswith(".zip/")) or
+                        (link_inputed.startswith("https://") and link_inputed.endswith(".zip/"))):
                     # Check if the link is valid
                     if self.__is_link_valid(link_inputed):
+                        # Posicao .zip e da ultima barra
+                        posicao_ponto_zip = link_inputed.find(".zip")
+                        posicao_ultima_barra = link_inputed.rfind("/", 0,
+                                                                  len(link_inputed) - 2 if link_inputed.endswith(
+                                                                      "/") else len(link_inputed))
+                        # Seta nome arquivo
+                        self.__filename = link_inputed[posicao_ultima_barra + 1:posicao_ponto_zip]
+                        # Seta link
                         self.__link = link_inputed
                         break
                     else:
@@ -33,5 +43,6 @@ class ZipDownload:
                     # raise error
                     raise Exception("Error link invalido! \n O link deve comecar com http:// or https:// e possuir um "
                                     "arquivo .zip")
+                # check if the link starts with http:// or https://
             except Exception as e:
                 print(e)
